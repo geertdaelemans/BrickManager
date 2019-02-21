f6b0a2d628bcf976b3f36e0770be64c699e872ae
Geert Daelemans <geert.daelemans@vrt.be>
2019-02-21 10:49:46 +0100

    Added automatic calculation of Total Cost.

--- generictablemodel.cpp
+++ generictablemodel.cpp
@@ -8,11 +8,19 @@ GenericTableModel::GenericTableModel(QObject *parent, QSqlDatabase db)
 
 QVariant GenericTableModel::data(const QModelIndex &index, int role) const
 {
-    // Format Date and Time
     if (role == Qt::DisplayRole){
+        // Format Date and Time
         if(index.column() == fieldIndex("date_ordered") ||
            index.column() == fieldIndex("date_created"))
             return QVariant(qvariant_cast<QDateTime>(QSqlTableModel::data(index, role)).toString(Qt::SystemLocaleShortDate));
+
+        // Calculate total
+        if(index.column() == fieldIndex("Total")) {
+            int quantity = data(this->index(index.row(),fieldIndex("Qty"))).toInt();
+            double price = data(this->index(index.row(),fieldIndex("Price"))).toDouble();;
+            return QVariant(price * quantity);
+        }
+
     }
     return QSqlTableModel::data(index, role);
 }
